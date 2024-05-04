a463d7170dbcff6d1fcd174efef0babc93ee0d12
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-05-04 13:02:38 +0600

    get-menu data api

--- index.js
+++ index.js
@@ -24,13 +24,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const menuCollection = client.db('restoDB').collection('menu')
+
+    // menu all data get database 
+    app.get('/menu',async(req,res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
